08461aec2416b08bdabb168ce1571048609ba835
Chennan Nicholas Mao <garudaomega@gmail.com>
2017-12-05 14:58:53 -0800

    import fix

--- app/src/main/java/com/garmega/metrofit_sample/complex/retrofit_interfaces/RetrofitUserManagementService.kt
+++ app/src/main/java/com/garmega/metrofit_sample/complex/retrofit_interfaces/RetrofitUserManagementService.kt
@@ -2,7 +2,7 @@ package com.garmega.metrofit_sample.complex.retrofit_interfaces
 
 import com.garmega.metrofit_sample.RetrofitInitialization
 import com.garmega.metrofit_sample.complex.response_models.TeamResponse
-import com.garmega.metrofit_sample.simple.response_models.UserResponse
+import com.garmega.metrofit_sample.complex.response_models.UserResponse
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import retrofit2.Call
