f370dbc0b2c7a3b238a1b9adb0f753fcccea82d5
Chennan Nicholas Mao <garudaomega@gmail.com>
2018-01-03 16:30:39 -0800

    added debug tag and todo list

--- app/src/main/java/com/garmega/metrofit_sample/simple/WeatherServiceManager.kt
+++ app/src/main/java/com/garmega/metrofit_sample/simple/WeatherServiceManager.kt
@@ -11,6 +11,14 @@ import retrofit2.Response
 
 /**
  * Created by Nick on 12/2/17.
+ *
+ * TODO: Obfuscate and renew APIKey.
+ * TODO: Display NO_TAG when no debug tag is provided
+ * TODO: Enhance debugging logs with on/off. Per method and overall
+ * TODO: Implement UI elements to respond to UINotifier.
+ * TODO: Start processing and displaying received weather information.
+ *
+ * TODO: Cleanup global namespace by moving managers into APICaller.
  */
 
 class WeatherServiceManager(private val retrofitWeatherService: RetrofitWeatherService) {
@@ -48,7 +56,7 @@ class WeatherServiceManager(private val retrofitWeatherService: RetrofitWeatherS
     private fun getWeather(cityName: String, receiver: ResponseReceiver) {
         val call = retrofitWeatherService.getWeather(cityName, "52999c99f1a294b470e1e57f2602a5e1")
 
-        val callback = object: ResponseCallback<WeatherDataResponse>(receiver, "") {
+        val callback = object: ResponseCallback<WeatherDataResponse>(receiver, "GET_WEATHER") {
             override fun performIntake(body: WeatherDataResponse?, outboundFreight: Map<String, Any>) {
                 super.performIntake(body, outboundFreight)
             }
